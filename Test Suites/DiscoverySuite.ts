01935e7dd65302cd47f2741b26c3b623014c0dd3
sjain1 <sjain1@LDCL313952.dne.dci.discovery.com>
2018-10-01 22:10:17 +0100

    aaa

---
+++ Test Suites/DiscoverySuite.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>DiscoverySuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>0178f2c2-93ca-4f4d-8324-0b216f04d81b</testSuiteGuid>
+   <testCaseLink>
+      <guid>07a92265-db4a-4900-983b-c56ffe10c989</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/WorkspaceRegression</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5184bfd6-723f-48eb-9bca-97d79f5e63fb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SearchFunction</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
