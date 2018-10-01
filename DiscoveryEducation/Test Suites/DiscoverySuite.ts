bae2611bb0f3d6979dad5b418a8e507d583bc4bf
Saif Jain <saif_jain@discovery.com>
2018-10-01 15:53:11 +0100

    new project

---
+++ DiscoveryEducation/Test Suites/DiscoverySuite.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>DiscoverySuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-10-01T14:41:06</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>ddf4a16d-489a-40a2-91c2-d32855dca6c4</testSuiteGuid>
+   <testCaseLink>
+      <guid>07a92265-db4a-4900-983b-c56ffe10c989</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/WorkspaceRegression</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5184bfd6-723f-48eb-9bca-97d79f5e63fb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/SearchFunction</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
